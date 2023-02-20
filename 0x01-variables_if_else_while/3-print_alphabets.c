5b2e6785ebe796c84f96108b7fdcdcbc8b704e9c
Sauceboy7 <adebiyifavour@7gmail.com>
2023-02-20 05:53:26 -0800

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: Always 0;
+*/
+
+int main(void)
+{
+int m = 97;
+int n = 65;
+
+while (m < 123)
+{
+putchar(m);
+m++;
+}
+while (n < 91)
+{
+putchar(n);
+n++;
+}
+putchar(10);
+return (0);
+}
+
