711c20bde66bbd82697c3cfeef6c8aa343703fc2
Sauceboy7 <adebiyifavour@7gmail.com>
2023-02-23 16:05:58 -0800

    task 1

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,5 @@
-#ifndef MAIN_H
-
-#define MAIN_H
+#ifndef main_h_
+#define main_h_
 
 #include <stdio.h>
 
@@ -9,4 +8,4 @@ int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
-#endif /* MAIN_H */
+#endif
